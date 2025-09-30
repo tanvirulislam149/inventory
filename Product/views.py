f1801440a287d1a4b789a5619a1c8f174af6b1d4
tanvir <tanvirulislam149@gmail.com>
2025-10-01 00:55:29 +0600

    urls done

--- Product/views.py
+++ Product/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from Product.models import Product, Category, Supplier
+from Product.serializers import CategorySerializer, ProductSerializer, SupplierSerializer
 
 # Create your views here.
+
+class CategoryViewsets(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class SupplierViewsets(ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+
+
+class ProductViewsets(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
