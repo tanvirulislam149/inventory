147721aab6e44c139fbf5d59ae9373d99a87f3ee
tanvir <tanvirulislam149@gmail.com>
2025-10-01 00:21:37 +0600

    models done

--- Product/models.py
+++ Product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
@@ -6,5 +7,25 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
 
-# class Product(models.Model):
-#     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name 
+
+class Supplier(models.Model):
+    company_name = models.CharField(max_length=100)
+    person_name = models.CharField(max_length=100)
+    phone = PhoneNumberField()
+    email = models.EmailField(max_length=254)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.person_name 
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    supplier = models.ManyToManyField(Supplier, related_name="products")
+
+    def __str__(self):
+        return self.name
