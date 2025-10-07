63a304b11e5243e9e52fdf1cff7d485f66b341f4
tanvir <tanvirulislam149@gmail.com>
2025-10-07 21:07:18 +0600

    model register in admin

--- Product/admin.py
+++ Product/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from Product.models import Product
+from Product.models import Product, Category, Supplier
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Supplier)
