8ff954f2899aad7d9ad10abbfd44c5cc7e096dbb
tanvir <tanvirulislam149@gmail.com>
2025-10-07 22:33:58 +0600

    stock fixed

--- Stock/views.py
+++ Stock/views.py
@@ -3,6 +3,8 @@ from rest_framework.viewsets import ModelViewSet
 from Stock.models import Stock
 from Product.models import Product
 from Stock.serializers import StockSerializer, CreateStockSerializer
+from rest_framework.exceptions import ValidationError
+from django.db import transaction
 
 # Create your views here.
 class StockViewset(ModelViewSet):
@@ -16,13 +18,17 @@ class StockViewset(ModelViewSet):
             return StockSerializer
         
     def perform_create(self, serializer):
-        serializer.save()
-        data = serializer.data
-        product = Product.objects.filter(id=data["product"]).first()
-        print("product", product)
-        if data["movement_type"] == "IN":
-            product.stock_quantity = product.stock_quantity + data["quantity"]
-        elif data["movement_type"] == "OUT":
-            product.stock_quantity = product.stock_quantity - data["quantity"]
-            
-        product.save()
+        with transaction.atomic():
+            serializer.save()
+            data = serializer.data
+            product = Product.objects.filter(id=data["product"]).first()
+            print("product", product)
+            if data["movement_type"] == "IN":
+                product.stock_quantity = product.stock_quantity + data["quantity"]
+            elif data["movement_type"] == "OUT":
+                if product.stock_quantity < data["quantity"]:
+                    print("check quantity")
+                    raise ValidationError(f"The requested quantity of this product is not available. Only {product.stock_quantity} product is available.")
+                product.stock_quantity = product.stock_quantity - data["quantity"]
+
+            product.save()
