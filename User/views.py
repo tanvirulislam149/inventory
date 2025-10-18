58c8d147d45b4c79ee0ce39eab6d1cae0a760482
tanvir <tanvirulislam149@gmail.com>
2025-10-18 15:40:57 +0600

    permission added

--- User/views.py
+++ User/views.py
@@ -3,12 +3,14 @@ from django.contrib.auth import get_user_model
 from rest_framework.viewsets import ModelViewSet
 from User.serializers import MyGroupSerializer, MyPermissionSerializer, CreateMyGroupSerializer
 from django.contrib.auth.models import Group, Permission
+from Core.permissions import IsOwner
 
 # Create your views here.
 User = get_user_model()
 
 class MyGroupViewset(ModelViewSet): 
     queryset = Group.objects.all()
+    permission_classes = [IsOwner]
     
     def get_serializer_class(self):
         if self.request.method in ["POST", "PUT", "PATCH"]:
@@ -17,5 +19,7 @@ class MyGroupViewset(ModelViewSet):
             return MyGroupSerializer
 
 class MyPermissionViewset(ModelViewSet): 
+    http_method_names = ["get", "head", "options"]
     queryset = Permission.objects.all()
     serializer_class = MyPermissionSerializer
+    permission_classes = [IsOwner]
