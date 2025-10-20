42db46785f3c0ba0e4563a26442bd17273d52e05
tanvir <tanvirulislam149@gmail.com>
2025-10-20 11:20:18 +0600

    stock status done

--- Product/models.py
+++ Product/models.py
@@ -22,12 +22,28 @@ class Supplier(models.Model):
         return f"{self.company_name} | {self.person_name}" 
 
 class Product(models.Model):
+    STOCK_STATUS_CHOICES = {
+        "out_of_stock": "out_of_stock",
+        "low_stock": "low_stock",
+        "in_stock": "in_stock"
+    }
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_quantity = models.PositiveIntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name="products")
     image = CloudinaryField("image", default="default_mlcgud")
+    stock_status = models.CharField(max_length=30, choices=STOCK_STATUS_CHOICES, default="out_of_stock")
+
+    def save(self, *args, **kwargs):
+        # Automatically update stock status based on quantity
+        if self.stock_quantity == 0:
+            self.stock_status = "out_of_stock"
+        elif self.stock_quantity < 15:
+            self.stock_status = "low_stock"
+        else:
+            self.stock_status = "in_stock"
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name} X {self.supplier.company_name}"
